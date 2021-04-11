6a3d51afe89e5e4824b7f981f319edf66f9ffcd5
darius-aghili <github@aghili-online.de>
2021-04-11 21:35:46 +0200

    Adding django-boostrap4

--- AGTCheckWebsite/settings.py
+++ AGTCheckWebsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'AGTCheckController.apps.AgtcheckcontrollerConfig',
+    'django-bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
