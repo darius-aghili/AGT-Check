e54469693ad67c0d0909f295df08d037cde64a42
darius-aghili <github@aghili-online.de>
2021-04-11 22:44:49 +0200

    Add index.html

--- AGTCheckController/views.py
+++ AGTCheckController/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'index.html')
